6f1b38d86819a78cf071d36a3d78c09aae15d832
Marie Valmori <38396602+Marievlm@users.noreply.github.com>
2021-03-27 10:46:23 +0100

    Update Useful_Package.R

--- Useful_Package.R
+++ Useful_Package.R
@@ -1,15 +1,15 @@
-# fonction pour ne pas installer des packgaes déjà installés sur la machine
+# fonction pour ne pas installer des packgaes dÃ©jÃ  installÃ©s sur la machine
 Useful_Package <- function(packages_Vec) # packages_Vec est un vecteur de noms de pakage ex : packages_Vec <- c("ggplot2", "plyr", "tidyr")
 {
   i = 1
-  for (i in length(packages_Vec)){
+  for (i in 1:length(packages_Vec)){
     if (!requireNamespace(packages_Vec[i], quietly = TRUE))
     {
       install.packages(packages_Vec[i])
-      sprintf("Le package %s vient d'être installé.", packages_Vec[i])
+      sprintf("Le package %s vient d'Ãªtre installÃ©.", packages_Vec[i])
     }
     else {
-      sprintf("Le package %s est déjà installé.", packages_Vec[i])
+      sprintf("Le package %s est dÃ©jÃ  installÃ©.", packages_Vec[i])
       i = i+1}
   }
 }
