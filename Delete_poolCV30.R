8bcbae6beedd72c72ee1ab6731210b0509918b5e
Marie Valmori <38396602+Marievlm@users.noreply.github.com>
2021-03-29 22:29:56 +0200

    Update Delete_poolCV30.R

--- Delete_poolCV30.R
+++ Delete_poolCV30.R
@@ -1,14 +1,14 @@
-###### Calcul du CV pour les l'intensités des pools généréés par le package XCMS ########
+###### Calcul du CV pour les l'intensitÃ©s des pools gÃ©nÃ©rÃ©Ã©s par le package XCMS ########
 
 ## Appel des librairies
-library("xlsx")
+library('xlsx')
 library('stringr')
 library('filesstrings')
 
-## ouverture du fichier
+## Ouverture du fichier intensitÃ© gÃ©nÃ©rÃ© par XCMS
 
-Intensity_Neg <- read.table(file = 'Neg_IRSN_CorCer.tsv', sep = '\t', header = TRUE) # préciser le nom du fichier .tsv à importer
-Vec_pool <- as.vector(which(str_detect(colnames(Intensity_Neg), "pool"))) # prendre les connes uniquement pour pool
+Intensity_Neg <- read.table(file = 'Neg_IRSN_CorCer.tsv', sep = '\t', header = TRUE) # prÃ©ciser le nom du fichier '.tsv' Ã  importer dans le dossier de travail
+Vec_pool <- as.vector(which(str_detect(colnames(Intensity_Neg), "pool"))) # prendre les colonnes 'pool'
 pool <- Intensity_Neg[,min(Vec_pool):max(Vec_pool)] # faire un tableau qui contient uniquement les colonnes pools
 
 ## Fonction pour calculer CV
@@ -17,19 +17,20 @@ CV <- function(x) {
   return(CV)
 }
 
-## Applique la fonciton CV sur toutes les colonnes poools
-Coeff_Var <- apply(pool, MARGIN = 1,CV) # fonction pour déterminer le CV de toutes les colonnes
-name <- sprintf("%03d.png", Intensity_Neg[,1]) # permet de mettre des 0 dansle nom des fichiers pour l'extension pngg, correspond donc au titre des fichiers présents dans les dossiers
+## Applique la fonction 'CV' sur toutes les colonnes pools
+Coeff_Var <- apply(pool, MARGIN = 1,CV) # fonction pour dÃ©terminer le CV de toutes les colonnes
+name <- sprintf("%03d.png", Intensity_Neg[,1]) # permet de mettre des 0 dans le nom des fichiers puis l'extension 'png', 
+                                               # correspond donc aux titrex des fichiers prÃ©sents dans les dossiers gÃ©nÃ©rÃ©s par XCMS
 
 CpoolCV <-cbind(name,Coeff_Var, pool)
-Select_pool <- subset(CpoolCV, Coeff_Var <= 30)
+Select_pool <- subset(CpoolCV, Coeff_Var <= 30) #selectionne les features avec un CV <= 30%
 
 
-## Deplace les fichiers avec les CV<30
+## Deplace les fichiers correspondant aux features avec des CV<=30
 fn <- as.vector(Select_pool$name)
 # Check son existence dans le dossier de travail
 if (file.exists(fn)) {
-  # Deplace le fichier dans le dossier spcifié
+  # Deplace le fichier dans le dossier spcifiÃ©
   filesstrings::file.move(fn, "EIC_CVinf30")
 }
 
